50aa3c9b62e8302e888beeee3424771e02b21123
Somodi Viktor <somodi.viktor@gmail.com>
2016-04-08 06:41:22 +0200

    feat(response-factory): #createSuccess, #createError

---
+++ lib/response-factory/index.ts
@@ -0,0 +1,28 @@
+import { Response, ResponseOptions } from 'angular2/http';
+
+export class ResponseFactory {
+
+  createSuccess({
+    status = 200,
+    message = {}
+  }) {
+    return this._create({ status, message });
+  }
+
+
+  createError({
+    status = 400,
+    message = 'Something went wrong!'
+  } = {}) {
+    return this._create({ status, message });
+  }
+
+
+  _create({ status, message }) {
+    return new Response(new ResponseOptions({
+      body: { message },
+      status
+    }));
+  }
+
+}
